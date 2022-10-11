e9d1096ae08d0a84fc28b1186f361dcbd2f55357
RyoyaKawano <ryoya_kawano@live.jp>
2022-10-11 13:08:45 +0900

    [Update] 名前が空欄のときの処理

--- omikuji.js
+++ omikuji.js
@@ -2,7 +2,11 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えてください。");
-document.getElementById("name").innerHTML = username;
+if(username == ""){
+    document.getElementById("name").innerHTML = "名無し";
+}else{
+    document.getElementById("name").innerHTML = username;
+}
 
 let rand = Math.floor(Math.random() * 5);
 if(rand == 0){
